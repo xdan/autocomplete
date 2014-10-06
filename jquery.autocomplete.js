332d0fce37ad8596fd9b2362cb7202d7d9a59631
Valeriy <skoder@ya.ru>
2014-10-06 12:58:12 +0600

    Bower manifest update

--- jquery.autocomplete.js
+++ jquery.autocomplete.js
@@ -1,5 +1,5 @@
 /**
- * @preserve jQuery Autocomplete plugin v1.0.5
+ * @preserve jQuery Autocomplete plugin v1.0.6
  * @homepage http://xdsoft.net/jqplugins/autocomplete/
  * (c) 2014, Chupurnov Valeriy <chupurnov@gmail.com>
  */
