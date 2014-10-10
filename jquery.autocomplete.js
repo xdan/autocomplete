c8eb64d97502f2c434949d4c88ef1a6316e9033e
Valeriy <skoder@ya.ru>
2014-10-10 11:54:31 +0600

    Support Accents. Fix resize plugin with resize window

--- jquery.autocomplete.js
+++ jquery.autocomplete.js
@@ -1,10 +1,85 @@
 /**
- * @preserve jQuery Autocomplete plugin v1.0.7
+ * @preserve jQuery Autocomplete plugin v1.0.8
  * @homepage http://xdsoft.net/jqplugins/autocomplete/
  * (c) 2014, Chupurnov Valeriy <chupurnov@gmail.com>
  */
 !function ($) {
 	'use strict';
+	var	ARROWLEFT = 37,
+		ARROWRIGHT = 39,
+		ARROWUP = 38,
+		ARROWDOWN = 40,
+		TAB = 9,
+		CTRLKEY = 17,
+		SHIFTKEY = 16,
+		DEL = 46,
+		ENTER = 13,
+		ESC = 27,
+		BACKSPACE = 8,
+		AKEY = 65,
+		CKEY = 67,
+		VKEY = 86,
+		ZKEY = 90,
+		YKEY = 89,
+		defaultSetting = {},
+		currentInput = false,
+		ctrlDown = false,
+		shiftDown = false,
+		interval_for_visibility,
+		accent_map = {
+			'ẚ':'a','Á':'a','á':'a','À':'a','à':'a','Ă':'a','ă':'a','Ắ':'a','ắ':'a','Ằ':'a','ằ':'a','Ẵ':'a','ẵ':'a','Ẳ':'a',
+			'Ẫ':'a','ẫ':'a','Ẩ':'a','ẩ':'a','Ǎ':'a','ǎ':'a','Å':'a','å':'a','Ǻ':'a','ǻ':'a','Ä':'a','ä':'a','Ǟ':'a','ǟ':'a',
+			'Ã':'a','ã':'a','Ȧ':'a','ȧ':'a','Ǡ':'a','ǡ':'a','Ą':'a','ą':'a','Ā':'a','ā':'a','Ả':'a','ả':'a','Ȁ':'a','ȁ':'a',
+			'Ȃ':'a','ȃ':'a','Ạ':'a','ạ':'a','Ặ':'a','ặ':'a','Ậ':'a','ậ':'a','Ḁ':'a','ḁ':'a','Ⱥ':'a','ⱥ':'a','Ǽ':'a','ǽ':'a',
+			'Ǣ':'a','ǣ':'a','Ḃ':'b','ḃ':'b','Ḅ':'b','ḅ':'b','Ḇ':'b','ḇ':'b','Ƀ':'b','ƀ':'b','ᵬ':'b','Ɓ':'b','ɓ':'b','Ƃ':'b',
+			'ƃ':'b','Ć':'c','ć':'c','Ĉ':'c','ĉ':'c','Č':'c','č':'c','Ċ':'c','ċ':'c','Ç':'c','ç':'c','Ḉ':'c','ḉ':'c','Ȼ':'c',
+			'ȼ':'c','Ƈ':'c','ƈ':'c','ɕ':'c','Ď':'d','ď':'d','Ḋ':'d','ḋ':'d','Ḑ':'d','ḑ':'d','Ḍ':'d','ḍ':'d','Ḓ':'d','ḓ':'d',
+			'Ḏ':'d','ḏ':'d','Đ':'d','đ':'d','ᵭ':'d','Ɖ':'d','ɖ':'d','Ɗ':'d','ɗ':'d','Ƌ':'d','ƌ':'d','ȡ':'d','ð':'d','É':'e',
+			'Ə':'e','Ǝ':'e','ǝ':'e','é':'e','È':'e','è':'e','Ĕ':'e','ĕ':'e','Ê':'e','ê':'e','Ế':'e','ế':'e','Ề':'e','ề':'e',
+			'Ễ':'e','ễ':'e','Ể':'e','ể':'e','Ě':'e','ě':'e','Ë':'e','ë':'e','Ẽ':'e','ẽ':'e','Ė':'e','ė':'e','Ȩ':'e','ȩ':'e',
+			'Ḝ':'e','ḝ':'e','Ę':'e','ę':'e','Ē':'e','ē':'e','Ḗ':'e','ḗ':'e','Ḕ':'e','ḕ':'e','Ẻ':'e','ẻ':'e','Ȅ':'e','ȅ':'e',
+			'Ȇ':'e','ȇ':'e','Ẹ':'e','ẹ':'e','Ệ':'e','ệ':'e','Ḙ':'e','ḙ':'e','Ḛ':'e','ḛ':'e','Ɇ':'e','ɇ':'e','ɚ':'e','ɝ':'e',
+			'Ḟ':'f','ḟ':'f','ᵮ':'f','Ƒ':'f','ƒ':'f','Ǵ':'g','ǵ':'g','Ğ':'g','ğ':'g','Ĝ':'g','ĝ':'g','Ǧ':'g','ǧ':'g','Ġ':'g',
+			'ġ':'g','Ģ':'g','ģ':'g','Ḡ':'g','ḡ':'g','Ǥ':'g','ǥ':'g','Ɠ':'g','ɠ':'g','Ĥ':'h','ĥ':'h','Ȟ':'h','ȟ':'h','Ḧ':'h',
+			'ḧ':'h','Ḣ':'h','ḣ':'h','Ḩ':'h','ḩ':'h','Ḥ':'h','ḥ':'h','Ḫ':'h','ḫ':'h','H':'h','̱':'h','ẖ':'h','Ħ':'h','ħ':'h',
+			'Ⱨ':'h','ⱨ':'h','Í':'i','í':'i','Ì':'i','ì':'i','Ĭ':'i','ĭ':'i','Î':'i','î':'i','Ǐ':'i','ǐ':'i','Ï':'i','ï':'i',
+			'Ḯ':'i','ḯ':'i','Ĩ':'i','ĩ':'i','İ':'i','i':'i','Į':'i','į':'i','Ī':'i','ī':'i','Ỉ':'i','ỉ':'i','Ȉ':'i','ȉ':'i',
+			'Ȋ':'i','ȋ':'i','Ị':'i','ị':'i','Ḭ':'i','ḭ':'i','I':'i','ı':'i','Ɨ':'i','ɨ':'i','Ĵ':'j','ĵ':'j','J':'j','̌':'j',
+			'ǰ':'j','ȷ':'j','Ɉ':'j','ɉ':'j','ʝ':'j','ɟ':'j','ʄ':'j','Ḱ':'k','ḱ':'k','Ǩ':'k','ǩ':'k','Ķ':'k','ķ':'k','Ḳ':'k',
+			'ḳ':'k','Ḵ':'k','ḵ':'k','Ƙ':'k','ƙ':'k','Ⱪ':'k','ⱪ':'k','Ĺ':'a','ĺ':'l','Ľ':'l','ľ':'l','Ļ':'l','ļ':'l','Ḷ':'l',
+			'ḷ':'l','Ḹ':'l','ḹ':'l','Ḽ':'l','ḽ':'l','Ḻ':'l','ḻ':'l','Ł':'l','ł':'l','̣':'l','Ŀ':'l',
+			'ŀ':'l','Ƚ':'l','ƚ':'l','Ⱡ':'l','ⱡ':'l','Ɫ':'l','ɫ':'l','ɬ':'l','ɭ':'l','ȴ':'l','Ḿ':'m','ḿ':'m','Ṁ':'m','ṁ':'m',
+			'Ṃ':'m','ṃ':'m','ɱ':'m','Ń':'n','ń':'n','Ǹ':'n','ǹ':'n','Ň':'n','ň':'n','Ñ':'n','ñ':'n','Ṅ':'n','ṅ':'n','Ņ':'n',
+			'ņ':'n','Ṇ':'n','ṇ':'n','Ṋ':'n','ṋ':'n','Ṉ':'n','ṉ':'n','Ɲ':'n','ɲ':'n','Ƞ':'n','ƞ':'n','ɳ':'n','ȵ':'n','N':'n',
+			'̈':'n','n':'n','Ó':'o','ó':'o','Ò':'o','ò':'o','Ŏ':'o','ŏ':'o','Ô':'o','ô':'o','Ố':'o','ố':'o','Ồ':'o',
+			'ồ':'o','Ỗ':'o','ỗ':'o','Ổ':'o','ổ':'o','Ǒ':'o','ǒ':'o','Ö':'o','ö':'o','Ȫ':'o','ȫ':'o','Ő':'o','ő':'o','Õ':'o',
+			'õ':'o','Ṍ':'o','ṍ':'o','Ṏ':'o','ṏ':'o','Ȭ':'o','ȭ':'o','Ȯ':'o','ȯ':'o','Ȱ':'o','ȱ':'o','Ø':'o','ø':'o','Ǿ':'o',
+			'ǿ':'o','Ǫ':'o','ǫ':'o','Ǭ':'o','ǭ':'o','Ō':'o','ō':'o','Ṓ':'o','ṓ':'o','Ṑ':'o','ṑ':'o','Ỏ':'o','ỏ':'o','Ȍ':'o',
+			'ȍ':'o','Ȏ':'o','ȏ':'o','Ơ':'o','ơ':'o','Ớ':'o','ớ':'o','Ờ':'o','ờ':'o','Ỡ':'o','ỡ':'o','Ở':'o','ở':'o','Ợ':'o',
+			'ợ':'o','Ọ':'o','ọ':'o','Ộ':'o','ộ':'o','Ɵ':'o','ɵ':'o','Ṕ':'p','ṕ':'p','Ṗ':'p','ṗ':'p','Ᵽ':'p','Ƥ':'p','ƥ':'p',
+			'P':'p','̃':'p','p':'p','ʠ':'q','Ɋ':'q','ɋ':'q','Ŕ':'r','ŕ':'r','Ř':'r','ř':'r','Ṙ':'r','ṙ':'r','Ŗ':'r',
+			'ŗ':'r','Ȑ':'r','ȑ':'r','Ȓ':'r','ȓ':'r','Ṛ':'r','ṛ':'r','Ṝ':'r','ṝ':'r','Ṟ':'r','ṟ':'r','Ɍ':'r','ɍ':'r','ᵲ':'r',
+			'ɼ':'r','Ɽ':'r','ɽ':'r','ɾ':'r','ᵳ':'r','ß':'s','Ś':'s','ś':'s','Ṥ':'s','ṥ':'s','Ŝ':'s','ŝ':'s','Š':'s','š':'s',
+			'Ṧ':'s','ṧ':'s','Ṡ':'s','ṡ':'s','ẛ':'s','Ş':'s','ş':'s','Ṣ':'s','ṣ':'s','Ṩ':'s','ṩ':'s','Ș':'s','ș':'s','ʂ':'s',
+			'S':'s','̩':'s','s':'s','Þ':'t','þ':'t','Ť':'t','ť':'t','T':'t','ẗ':'t','Ṫ':'t','ṫ':'t','Ţ':'t','ţ':'t','Ṭ':'t',
+			'ṭ':'t','Ț':'t','ț':'t','Ṱ':'t','ṱ':'t','Ṯ':'t','ṯ':'t','Ŧ':'t','ŧ':'t','Ⱦ':'t','ⱦ':'t','ᵵ':'t',
+			'ƫ':'t','Ƭ':'t','ƭ':'t','Ʈ':'t','ʈ':'t','ȶ':'t','Ú':'u','ú':'u','Ù':'u','ù':'u','Ŭ':'u','ŭ':'u','Û':'u','û':'u',
+			'Ǔ':'u','ǔ':'u','Ů':'u','ů':'u','Ü':'u','ü':'u','Ǘ':'u','ǘ':'u','Ǜ':'u','ǜ':'u','Ǚ':'u','ǚ':'u','Ǖ':'u','ǖ':'u',
+			'Ű':'u','ű':'u','Ũ':'u','ũ':'u','Ṹ':'u','ṹ':'u','Ų':'u','ų':'u','Ū':'u','ū':'u','Ṻ':'u','ṻ':'u','Ủ':'u','ủ':'u',
+			'Ȕ':'u','ȕ':'u','Ȗ':'u','ȗ':'u','Ư':'u','ư':'u','Ứ':'u','ứ':'u','Ừ':'u','ừ':'u','Ữ':'u','ữ':'u','Ử':'u','ử':'u',
+			'Ự':'u','ự':'u','Ụ':'u','ụ':'u','Ṳ':'u','ṳ':'u','Ṷ':'u','ṷ':'u','Ṵ':'u','ṵ':'u','Ʉ':'u','ʉ':'u','Ṽ':'v','ṽ':'v',
+			'Ṿ':'v','ṿ':'v','Ʋ':'v','ʋ':'v','Ẃ':'w','ẃ':'w','Ẁ':'w','ẁ':'w','Ŵ':'w','ŵ':'w','W':'w','̊':'w','ẘ':'w','Ẅ':'w',
+			'ẅ':'w','Ẇ':'w','ẇ':'w','Ẉ':'w','ẉ':'w','Ẍ':'x','ẍ':'x','Ẋ':'x','ẋ':'x','Ý':'y','ý':'y','Ỳ':'y','ỳ':'y','Ŷ':'y',
+			'ŷ':'y','Y':'y','ẙ':'y','Ÿ':'y','ÿ':'y','Ỹ':'y','ỹ':'y','Ẏ':'y','ẏ':'y','Ȳ':'y','ȳ':'y','Ỷ':'y','ỷ':'y',
+			'Ỵ':'y','ỵ':'y','ʏ':'y','Ɏ':'y','ɏ':'y','Ƴ':'y','ƴ':'y','Ź':'z','ź':'z','Ẑ':'z','ẑ':'z','Ž':'z','ž':'z','Ż':'z',
+			'ż':'z','Ẓ':'z','ẓ':'z','Ẕ':'z','ẕ':'z','Ƶ':'z','ƶ':'z','Ȥ':'z','ȥ':'z','ʐ':'z','ʑ':'z','Ⱬ':'z','ⱬ':'z','Ǯ':'z',
+			'ǯ':'z','ƺ':'z','２':'2','６':'6','Ｂ':'B','Ｆ':'F','Ｊ':'J','Ｎ':'N','Ｒ':'R','Ｖ':'V','Ｚ':'Z','ｂ':'b','ｆ':'f','ｊ':'j',
+			'ｎ':'n','ｒ':'r','ｖ':'v','ｚ':'z','１':'1','５':'5','９':'9','Ａ':'A','Ｅ':'E','Ｉ':'I','Ｍ':'M','Ｑ':'Q','Ｕ':'U','Ｙ':'Y',
+			'ａ':'a','ｅ':'e','ｉ':'i','ｍ':'m','ｑ':'q','ｕ':'u','ｙ':'y','０':'0','４':'4','８':'8','Ｄ':'D','Ｈ':'H','Ｌ':'L','Ｐ':'P',
+			'Ｔ':'T','Ｘ':'X','ｄ':'d','ｈ':'h','ｌ':'l','ｐ':'p','ｔ':'t','ｘ':'x','３':'3','７':'7','Ｃ':'C','Ｇ':'G','Ｋ':'K','Ｏ':'O',
+			'Ｓ':'S','Ｗ':'W','ｃ':'c','ｇ':'g','ｋ':'k','ｏ':'o','ｓ':'s','ｗ':'w','ẳ':'a','Â':'a','â':'a','Ấ':'a','ấ':'a','Ầ':'a','ầ':'a'
+		};
+
 	if (window.getComputedStyle === undefined) {
 		window.getComputedStyle = (function () {
 			function getPixelSize(element, style, property, fontSize) {
@@ -78,26 +153,7 @@
 			return getComputedStyle;
 		}(this));
 	}
-	var	ARROWLEFT = 37,
-		ARROWRIGHT = 39,
-		ARROWUP = 38,
-		ARROWDOWN = 40,
-		TAB = 9,
-		CTRLKEY = 17,
-		SHIFTKEY = 16,
-		DEL = 46,
-		ENTER = 13,
-		ESC = 27,
-		BACKSPACE = 8,
-		AKEY = 65,
-		CKEY = 67,
-		VKEY = 86,
-		ZKEY = 90,
-		YKEY = 89,
-		defaultSetting = {},
-		currentInput = false,
-		ctrlDown = false,
-		shiftDown = false;
+
 
 	$(document)
 		.on('keydown.xdsoftctrl', function (e) {
@@ -116,7 +172,20 @@
 				ctrlDown = false;
 			}
 		});
-
+	
+	function accentReplace (s) {
+		if (!s) { return ''; }
+		var ret = '',i;
+		for (i=0; i < s.length; i+=1) {
+			ret += accent_map[s.charAt(i)] || s.charAt(i);
+		}
+		return ret;
+	}
+	
+	function escapeRegExp (str) {
+		return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+	}
+	
 	function getCaretPosition(input) {
 		if (!input) {
 			return;
@@ -378,7 +447,10 @@
 		closeOnBlur: true,
 
 		autoselect: false,
-
+		
+		accents: true,
+		replaceAccentsForRemote: true,
+		
 		limit: 20,
 
 		timeoutUpdate: 10,
@@ -388,7 +460,10 @@
 		},
 		
 		replace: [
-			function( url,query ){
+			function (url, query) {
+				if (this.replaceAccentsForRemote) {
+					query = accentReplace(query);
+				}
 				return url.replace('%QUERY%',encodeURIComponent(query));
 			}
 		],
@@ -403,7 +478,6 @@
 				
 				for(var i in items){
 					value = __safe.call(this,'getValue',source,[items[i],source]);
-					// right side equal
 					if( __safe.call(this,'equal',source,[value,query,source],false) ){
 						return items[i];
 					}
@@ -414,22 +488,24 @@
 		],
 		
 		valid:[
-			function ( value,query ){
-				try{
-					return value.toLowerCase().indexOf(query.toLowerCase())!=-1;
-				}catch( e ){
-					console.log(e);
-				} 
+			function (value, query) {
+				if (this.accents) {
+					value = accentReplace(value);
+					query = accentReplace(query);
+				}
+				return value.toLowerCase().indexOf(query.toLowerCase())!=-1;
+				
 			}
 		],
 		
 		filter:[
-			function ( items,query,source ){
-				var results = [],value = '';
-				for(var i in items){
-					value = isset(items[i][this.get('valueKey',source)])?items[i][this.get('valueKey',source)]:items[i].toString();
-					if( __safe.call(this,'valid',source,[value,query])  )
+			function (items, query, source) {
+				var results = [], value = '',i;
+				for (i = 0;i < items.length;i += 1) {
+					value = isset(items[i][this.get('valueKey', source)]) ? items[i][this.get('valueKey', source)] : items[i].toString();
+					if (__safe.call(this, 'valid', source, [value, query])) {
 						results.push(items[i]); 
+					}
 				}
 				return results;
 			}
@@ -452,12 +528,40 @@
 		],
 		
 		render:[
-			function( item,source,pid,query ){
-				var value = isset(item[this.get('valueKey',source)])?item[this.get('valueKey',source)]:item.toString(),
-					title = (isset(item[this.get('titleKey',source)])?item[this.get('titleKey',source)]:value)+'';
+			function (item, source, pid, query) {
+				var value = isset(item[this.get('valueKey', source)]) ? item[this.get('valueKey', source)] : item.toString(),
+					title = (isset(item[this.get('titleKey', source)]) ? item[this.get('titleKey', source)] : value) + '',
+					_value = '',
+					_query = '',
+					_title = '',
+					hilite_hints = '',
+					highlighted = '',
+					c, h, i,
+					spos = 0;
 					
-				if( this.highlight )
-					title = title.replace(RegExp('('+query.replace(/([\[\]\\(\)])/g,'\\$1')+')','i'),'<b>$1</b>');
+				if (this.highlight) {
+					if (!this.accents) {
+						title = title.replace(new RegExp('('+escapeRegExp(query)+')','i'),'<b>$1</b>');
+					}else{
+						_title = accentReplace(title).toLowerCase().replace(/[<>]+/g, ''),
+						_query = accentReplace(query).toLowerCase().replace(/[<>]+/g, '');
+						
+						hilite_hints = _title.replace(new RegExp(escapeRegExp(_query), 'g'), '<'+_query+'>');
+						for (i=0;i < hilite_hints.length;i += 1) {
+							c = title.charAt(spos);
+							h = hilite_hints.charAt(i);
+							if (h === '<') {
+								highlighted += '<b>';
+							} else if (h === '>') {
+								highlighted += '</b>';
+							} else {
+								spos += 1;
+								highlighted += c;
+							}
+						}
+						title = highlighted;
+					}
+				}
 					
 				return '<div '+(value==query?'class="active"':'')+' data-value="'+encodeURIComponent(value)+'">'
 							+title+
@@ -487,8 +591,8 @@
 		
 		$box
 			.css({
-				'display':$(that).css('display'),
-				'width':$(that).css('width')
+				'display':$input.css('display'),
+				'width':$input.css('width')
 			});
 		
 		if( options.style )
@@ -708,7 +812,10 @@
 			.on('updateHelperPosition.xdsoft',function(){
 				clearTimeout(timerUpdate);
 				timerUpdate = setTimeout(function(){
-
+					$box.css({
+						'display':$input.css('display'),
+						'width':$input.css('width')
+					});
 					$dropdown.css($.extend(true,{
 						left:$input.position().left,
 						top:$input.position().top+parseInt($input.css('marginTop'))+parseInt($input[0].offsetHeight),
@@ -784,9 +891,30 @@
 							.append($hint);
 					}
 				},options.timeoutUpdate||1);
-			})
-			.trigger('updateHelperPosition.xdsoft')
-			
+			});
+		
+		if ($input.is(':visible')) {
+			$input
+				.trigger('updateHelperPosition.xdsoft');
+		} else {
+			interval_for_visibility = setInterval(function () {
+				if ($input.is(':visible')) {
+					$input
+						.trigger('updateHelperPosition.xdsoft');
+					clearInterval(interval_for_visibility);
+				}
+			},100);
+		}
+		
+		$(window).on('resize',function () {
+			$box.css({
+				'width':'auto'
+			});
+			$input
+				.trigger('updateHelperPosition.xdsoft');
+		})
+		
+		$input	
 			.on('close.xdsoft',function(){
 				if( !iOpen )
 					return;
